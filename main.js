5a6ca2827b27b64f3b167c2325793aa4e96c2750
anas <anas@gmail.com>
2025-11-20 00:13:32 +0200

    create sum function

---
+++ main.js
@@ -0,0 +1,48 @@
+ const sum=(n1,n2) =>{
+    return n1+n2
+ }
+ console.log(sum(55,33));
+ //object 
+ const user={
+   firstname :"anas",
+   lastname :"ahmed",
+   age :26,
+   isadmin :false,
+   fav:["football"],
+   username :function(){
+      console.log({this:this});
+      
+      return this.firstname+ " "+this.lastname
+   }
+
+ }
+ console.log({user});
+ console.log(Object.values(user));
+ console.log(Object.keys(user));
+ console.log(user.age);
+ console.log(user['age']);
+ user.gender="male";
+ console.log({user});
+ console.log(user.username())
+ 
+ 
+ 
+ 
+ 
+ 
+//  const x=5;
+//  x=10;
+// const z=[50]
+// z=[90]
+// const w=[5];
+// console.log(w);
+const user ={age:25}
+user={age:30}
+user.age=30
+console.log({user});
+function sum (n1,n2) {
+   return n1+n2
+}
+
+
+ 
